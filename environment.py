c5a09144ee5cf91def880d5463114359227b3caa
kenny <kenny.kuo.fs@gmail.com>
2021-12-16 22:17:13 +0800

    saparate environment(previously board), adding muzero's config code(unfinished)

---
+++ environment.py
@@ -0,0 +1,113 @@
+from random import randint
+from copy import deepcopy
+bg   ="\x1b[48;5;"
+word ="\x1b[38;5;"
+end  ="m"
+reset="\x1b[0m"
+class Environment:
+	def __init__(self,board=None,score=None,g=None):
+		self.g=g
+		if board:#if it's not None
+			self.grid=deepcopy(board)
+		else:
+			self.init()
+		if score:
+			self.score=score
+		else:
+			self.score=0
+	def clear(self):
+		self.grid=[[0]*4 for _ in range(4)]
+	def init(self):
+		self.clear()
+		self.add()
+		self.add()
+		self.score=0
+	def movealine(self,line,reverse):#move from 3 to 0
+		if reverse:
+			line=line[::-1]#reverse
+		#move over all blank
+		index=1
+		while index<4:
+			forward=index-1
+			while forward>=0 and line[forward]==0:
+				line[forward]=line[forward+1]
+				line[forward+1]=0
+				forward-=1
+			index+=1
+		#combine
+		for i in range(3):
+			if line[i]==line[i+1] and line[i]>0:
+				line[i]+=1
+				line[i+1]=0
+				self.score+=2**line[i]
+		#move over blank(only pos 1,2 can be blank)
+		for i in range(1,2+1):
+			if line[i]==0:
+				line[i],line[i+1]=line[i+1],0
+		if reverse:
+			line=line[::-1]#reverse
+		return line
+	def step(self,drc):#up down left right
+		if drc==0:
+			for i in range(4):
+				self.grid[0][i],self.grid[1][i],self.grid[2][i],self.grid[3][i]=self.movealine([self.grid[0][i],self.grid[1][i],self.grid[2][i],self.grid[3][i]],False)
+		elif drc==1:
+			for i in range(4):
+				self.grid[0][i],self.grid[1][i],self.grid[2][i],self.grid[3][i]=self.movealine([self.grid[0][i],self.grid[1][i],self.grid[2][i],self.grid[3][i]],True)
+		elif drc==2:
+			for i in range(4):
+				self.grid[i][0],self.grid[i][1],self.grid[i][2],self.grid[i][3]=self.movealine([self.grid[i][0],self.grid[i][1],self.grid[i][2],self.grid[i][3]],False)
+		elif drc==3:
+			for i in range(4):
+				self.grid[i][0],self.grid[i][1],self.grid[i][2],self.grid[i][3]=self.movealine([self.grid[i][0],self.grid[i][1],self.grid[i][2],self.grid[i][3]],True)
+	def dump(self):
+		for i in self.grid:
+			for j in i:
+				if j>0:
+					print(bg+{1:'252'+end+word+'234',2:'222'+end+word+'234',3:'208',4:'202',5:'166',6:'196',7:'227',8:'190',9:'184',10:'184',11:'220',12:'14',}[j]+end+'%3d'%2**j+reset,end='')
+				else:
+					print('   ',end='')
+				print('|',end='')
+			print('\n----------------')
+		print('score=',self.score)
+	def finish(self):
+		for x in range(4):
+			for y in range(4):
+				if self.grid[x][y]==0:
+					return False
+				for dx,dy in ((1,0),(0,1),(-1,0),(0,-1)):
+					tx,ty=x+dx,y+dy
+					if tx>3 or ty>3 or tx<0 or ty<0:
+						continue
+					if self.grid[x][y]==self.grid[tx][ty]:
+						return False
+		return True
+	def legal_actions(self):#List[(x,y)]
+		result=[]
+		for x in range(4):
+			for y in range(4):
+				if self.grid[x][y]==0:
+					result.append((x,y))
+		return result
+	def add(self):
+		blank=self.legal_actions()
+		if len(blank)==0:
+			raise BaseException('no blank in grid (doing Environment.add)')
+		i=randint(0,len(blank)-1)
+		x,y=blank[i]
+		v=randint(1,10)
+		if v==1:#10%
+			v=2
+		else:
+			v=1
+		self.grid[x][y]=v
+		return x,y,v
+	def valid(self,action):
+		tmp=Environment(deepcopy(self.grid))
+		a=deepcopy(tmp.grid)
+		tmp.step(action)
+		for x in range(4):
+			for y in range(4):
+				if a[x][y]!=tmp.grid[x][y]:
+					return True
+		return False
--- main.py
+++ main.py
@@ -3,133 +3,162 @@ from copy import copy,deepcopy
 #from network import nn
 import numpy as np
 from time import time,sleep
+from environment import Environment
 bg   ="\x1b[48;5;"
 word ="\x1b[38;5;"
 end  ="m"
 reset="\x1b[0m"
-class Environment:
-	def __init__(self,board=None,score=None,g=None):
-		self.g=g
-		if board:#if it's not None
-			self.grid=deepcopy(board)
-		else:
-			self.init()
-		if score:
-			self.score=score
-		else:
-			self.score=0
-	def clear(self):
-		self.grid=[[0]*4 for _ in range(4)]
-	def init(self):
-		self.clear()
-		self.add()
-		self.add()
-		self.score=0
-	def movealine(self,line,reverse):#move from 3 to 0
-		if reverse:
-			line=line[::-1]#reverse
-		#move over all blank
-		index=1
-		while index<4:
-			forward=index-1
-			while forward>=0 and line[forward]==0:
-				line[forward]=line[forward+1]
-				line[forward+1]=0
-				forward-=1
-			index+=1
-		#combine
-		for i in range(3):
-			if line[i]==line[i+1] and line[i]>0:
-				line[i]+=1
-				line[i+1]=0
-				self.score+=2**line[i]
-		#move over blank(only pos 1,2 can be blank)
-		for i in range(1,2+1):
-			if line[i]==0:
-				line[i],line[i+1]=line[i+1],0
-		if reverse:
-			line=line[::-1]#reverse
-		return line
-	def step(self,drc):#up down left right
-		if drc==0:
-			for i in range(4):
-				self.grid[0][i],self.grid[1][i],self.grid[2][i],self.grid[3][i]=self.movealine([self.grid[0][i],self.grid[1][i],self.grid[2][i],self.grid[3][i]],False)
-		elif drc==1:
-			for i in range(4):
-				self.grid[0][i],self.grid[1][i],self.grid[2][i],self.grid[3][i]=self.movealine([self.grid[0][i],self.grid[1][i],self.grid[2][i],self.grid[3][i]],True)
-		elif drc==2:
-			for i in range(4):
-				self.grid[i][0],self.grid[i][1],self.grid[i][2],self.grid[i][3]=self.movealine([self.grid[i][0],self.grid[i][1],self.grid[i][2],self.grid[i][3]],False)
-		elif drc==3:
-			for i in range(4):
-				self.grid[i][0],self.grid[i][1],self.grid[i][2],self.grid[i][3]=self.movealine([self.grid[i][0],self.grid[i][1],self.grid[i][2],self.grid[i][3]],True)
-	def dump(self):
-		for i in self.grid:
-			for j in i:
-				if j>0:
-					print(bg+{1:'252'+end+word+'234',2:'222'+end+word+'234',3:'208',4:'202',5:'166',6:'196',7:'227',8:'190',9:'184',10:'184',11:'220',12:'14',}[j]+end+'%3d'%2**j+reset,end='')
-				else:
-					print('   ',end='')
-				print('|',end='')
-			print('\n----------------')
-		print('score=',self.score)
-	def finish(self):
-		for x in range(4):
-			for y in range(4):
-				if self.grid[x][y]==0:
-					return False
-				for dx,dy in ((1,0),(0,1),(-1,0),(0,-1)):
-					tx,ty=x+dx,y+dy
-					if tx>3 or ty>3 or tx<0 or ty<0:
-						continue
-					if self.grid[x][y]==self.grid[tx][ty]:
-						return False
-		return True
-	def getblank(self)->List[(x,y)]:
-		result=[]
-		for x in range(4):
-			for y in range(4):
-				if self.grid[x][y]==0:
-					result.append((x,y))
-		return result
-	def add(self):
-		blank=self.getblank()
-		if len(blank)==0:
-			raise BaseException('no blank in grid (doing Environment.add)')
-		i=randint(0,len(blank)-1)
-		x,y=blank[i]
-		v=randint(1,10)
-		if v==1:#10%
-			v=2
-		else:
-			v=1
-		self.grid[x][y]=v
-		return x,y,v
-	def valid(self,action):
-		tmp=board(deepcopy(self.grid))
-		a=deepcopy(tmp.grid)
-		tmp.moveall(action)
-		for x in range(4):
-			for y in range(4):
-				if a[x][y]!=tmp.grid[x][y]:
-					return True
-		return False
+'''
+assumption:
+total training steps=1e5 or less
+'''
+def default_visit_softmax_temperature(num_moves,training_steps):
+	if training_steps < 50e3:
+	  return 1.0
+	elif training_steps < 75e3:
+	  return 0.5
+	else:
+	  return 0.25
+class Config:
+	def __init__(self,
+				action_space_size:int=4,
+				max_moves:int=1e5,#it can be infinity because any 2048 game is bound to end
+				discount:float=0.9,
+				dirichlet_alpha:float=0.3,
+				num_simulations:int=100,
+				batch_size:int=1024,
+				td_steps:int=15,#when calculating value target, bootstrapping td_steps steps next moves' rewards and value
+				num_actors:int=1000,
+				lr_init:float=0.1,
+				lr_decay_steps:float=35e3,
+				visit_softmax_temperature_fn=default_visit_softmax_temperature,
+				known_bounds:Optional[KnownBounds]=None):
+	### Self-Play
+	self.action_space_size=action_space_size
+	self.num_actors=num_actors
+
+	self.visit_softmax_temperature_fn=visit_softmax_temperature_fn
+	self.max_moves=max_moves
+	self.num_simulations=num_simulations
+	self.discount=discount
+
+	# Root prior exploration noise.
+	self.root_dirichlet_alpha=dirichlet_alpha
+	self.root_exploration_fraction=0.25
+
+	# UCB formula
+	self.pb_c_base=19652
+	self.pb_c_init=1.25
+
+	# If we already have some information about which values occur in the
+	# environment, we can use them to initialize the rescaling.
+	# This is not strictly necessary, but establishes identical behaviour to
+	# AlphaZero in board games.
+	self.known_bounds=known_bounds
+
+	###Training
+	self.training_steps=int(100e3)
+	self.checkpoint_interval=int(5e2)
+	self.window_size=int(1e6)
+	self.batch_size=batch_size
+	self.num_unroll_steps=5
+	self.td_steps=td_steps
+
+	self.weight_decay=1e-4
+	self.momentum=0.9
+
+	#Exponentiallearningrateschedule
+	self.lr_init=lr_init
+	self.lr_decay_rate=0.1
+	self.lr_decay_steps=lr_decay_steps
+
+	def new_game(self):
+		return Game(self.action_space_size,self.discount)
+class Action:
+	def __init__(self,index:int):#up,down,left,right
+		self.index = index
+	def __hash__(self):
+		return self.index
+	def __eq__(self, other):
+		return self.index == other.index
+	def __gt__(self, other):
+		return self.index > other.index
 class Game:
-	def __init__(self,action_space_size:int,discount:float,player='Human',modelinfo=None):
-		self.info={'player':player,'modelinfo':modelinfo}
+	# Game is not responsible for record game
+	def __init__(self,action_space_size:int,discount:float):
 		self.environment=Environment()  # Game specific environment.
 		self.history=[]
 		self.child_visits=[]
 		self.root_values=[]
 		self.action_space_size=action_space_size
 		self.discount=discount
+	def terminal(self)->bool:
+		# if the game ends
+		return self.environment.finish()
+	def legal_actions(self)->List[Action]:
+		# list of legal actions
+		return self.environment.legal_actions()
+
+	def apply(self,action:Action):
+		reward = self.environment.step(action)
+		self.rewards.append(reward)
+		self.history.append(action)
+
+	def store_search_statistics(self, root: Node):
+		sum_visits = sum(child.visit_count for child in root.children.values())
+		action_space = (Action(index) for index in range(self.action_space_size))
+		self.child_visits.append([
+				root.children[a].visit_count / sum_visits if a in root.children else 0
+				for a in action_space
+		])
+		self.root_values.append(root.value())
+
+	def make_image(self, state_index: int):
+		# Game specific feature planes.
+		return []
+
+	def make_target(self, state_index: int, num_unroll_steps: int, td_steps: int,
+									to_play: Player):
+		# The value target is the discounted root value of the search tree N steps
+		# into the future, plus the discounted sum of all rewards until then.
+		targets = []
+		for current_index in range(state_index, state_index + num_unroll_steps + 1):
+			bootstrap_index = current_index + td_steps
+			if bootstrap_index < len(self.root_values):
+				value = self.root_values[bootstrap_index] * self.discount**td_steps
+			else:
+				value = 0
+
+			for i, reward in enumerate(self.rewards[current_index:bootstrap_index]):
+				value += reward * self.discount**i	# pytype: disable=unsupported-operands
+
+			# For simplicity the network always predicts the most recently received
+			# reward, even for the initial representation network where we already
+			# know this reward.
+			if current_index > 0 and current_index <= len(self.rewards):
+				last_reward = self.rewards[current_index - 1]
+			else:
+				last_reward = 0
+
+			if current_index < len(self.root_values):
+				targets.append((value, last_reward, self.child_visits[current_index]))
+			else:
+				# States past the end of games are treated as absorbing states.
+				targets.append((0, last_reward, []))
+		return targets
+
+	def to_play(self) -> Player:
+		return Player()
+
+	def action_history(self) -> ActionHistory:
+		return ActionHistory(self.history, self.action_space_size)
 	def addrand(self,x,y,v):
-		self.moves.append((2,x,y,v))
+		self.history.append((2,x,y,v))
 	def step(self,action):
-		self.moves.append((1,action))
+		self.history.append((1,action))
 	def replay(self):
-		tmp=board([[0]*4 for i in range(4)],addwhenplay=False)
-		for i in self.moves:
+		tmp=Environment([[0]*4 for i in range(4)],addwhenplay=False)
+		for i in self.history:
 			sleep(0.5)
 			if i[0]==1:
 				tmp.step(i[1])
@@ -137,20 +166,14 @@ class Game:
 				tmp.grid[i[1]][i[2]]=i[3]
 			tmp.dump()
 			sleep(0.5)
-	def write(self,f):
-		with open(f,'w') as F:
-			F.write(str(self.data))
-			F.write('\n')
-			F.write(str(self.moves))
-	def read(self,f):#clear origin data, moves
-		with open(f,'r') as F:
-			self.data=eval(F.readline())
-			self.moves=eval(F.readline())
 def player():
-	g=game()
-	b=board(g=g)
+	g=Game()
+	b=Environment(g=g)
+	l=0
 	while not b.finish():
 		b.dump()
+		print('move count:',l)
+		l+=1
 		done=False
 		while not done:
 			valid=False
@@ -165,12 +188,6 @@ def player():
 				x,y,v=b.add()
 				g.addrand(x,y,v)
 				done=True
-		
 	b.dump()
 	g.write('test.sgf')
-#replay
-g=Game()
-g.read('test.sgf')
-g.replay()
-#play
 player()
